408779220b3048f186455187f5eb2bc1ba7f49b9
xzy <tnwl8279@gmail.com>
2022-08-23 20:18:05 +0900

    foldering & CameraMovement

---
+++ TheGivingTree_New/Assets/Scripts/Camera/CameraManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraManager : MonoBehaviour
+{  
+    public Transform target;
+    public Vector3 cameraPos;
+
+    [Space(10f)]
+    public float cameraSpeed = 10f;
+    
+    Vector3 TargetPos;
+    
+    void FixedUpdate()
+    {
+        //플레이어 위치 + 타겟으로부터의 카메라 위치
+        TargetPos = new Vector3(target.position.x+cameraPos.x
+                                , target.position.y + cameraPos.y
+                                , target.position.z + cameraPos.z);
+
+        //선형보간하여 카메라에 적용
+        transform.position = Vector3.Lerp(transform.position, TargetPos, Time.deltaTime * cameraSpeed);
+    }
+}
---
+++ TheGivingTree_New/Assets/Scripts/Player/Player.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(PlayerController))]
+public class Player : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+    PlayerController controller;
+
+    void Start()
+    {
+        controller = GetComponent<PlayerController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //방향에 대한 입력 받기
+        Vector3 moveInput = new Vector3(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"), 0);
+        //벡터 방향값 * 이동 속도
+        Vector3 moveVelocity = moveInput.normalized * moveSpeed;
+
+        controller.Move(moveVelocity);
+    }
+}
